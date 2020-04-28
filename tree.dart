c50e690a42ee6151d31b274f924cba4b42e9dd03
Efraim Rodrigues <efraimnaassom@gmail.com>
2020-04-27 21:37:39 -0300

    Implements left rotate method.

--- tree.dart
+++ tree.dart
@@ -2,6 +2,12 @@ import 'node.dart';
 
 class Tree {
   Node root;
+  static Node NIL;
+
+  Tree() {
+    NIL = new Node();
+    NIL.color = Color.BLACK;
+  }
 
   void insert (int value) {
 
@@ -9,6 +15,26 @@ class Tree {
 
   void delete (int value) {
     
+  }
+
+  void _leftRotate(Node x) {
+    Node y = x.right;
+    x.right = y.left;
+
+    if(y.left != NIL)
+      y.left.parent = x;
+
+    y.parent = x.parent; 
+
+    if(x.parent == NIL)
+      this.root = y;
+    else if(x == x.parent.left)
+      x.parent.left = y;
+    else
+      x.parent.right = y;
+
+    y.left = x;
+    x.parent = y;
   }
 }
 
